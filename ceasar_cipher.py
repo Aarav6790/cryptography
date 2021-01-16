e67df3d6b894940123fd277a3cdcf1b5cefa93a6
Aarav Singh <53440217+Aarav6790@users.noreply.github.com>
2021-01-16 21:32:40 +0530

    Add files via upload

---
+++ ceasar_cipher.py
@@ -0,0 +1,17 @@
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x',
+'y','z']
+inpt = input('>>>').lower()
+
+shift = int(input("Enter Shift: "))
+outpt = []
+for i in inpt:
+    if i == ' ':
+        outpt.append(' ')
+    else:
+        if i in letters:
+            index = letters.index(i)
+            final_index = index+shift
+            if final_index>25:
+                final_index=final_index-26
+            outpt.append(letters[final_index])
+print(outpt)
